97cc2716eedf410caf74af531c3974ab23b3e89a
hurch08 <hurchesonofori@gmail.com>
2022-04-10 06:45:18 -0700

    using puts to print information

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 #include <string.h>
 
 /**
- * This is the main function
+ * main - Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 1 if the number is positive. 0 otherwise
  */
 int main(void)
 {
