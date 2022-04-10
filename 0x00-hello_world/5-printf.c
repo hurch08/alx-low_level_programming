cda778507ffdd2f4fc45fc3e84b595d5a282966c
hurch08 <hurchesonofori@gmail.com>
2022-04-10 07:26:32 -0700

    using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *
+ * main - prints "with proper grammar, but the outcome is a piece of art,\n"
+ *
+ * Return: 0 if success. 1 otherwise
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(str);
+
+	return (0);
+}
