244fee21fd42cab92fe0361450facc9f33a42202
hurch08 <hurchesonofori@gmail.com>
2022-04-10 06:23:51 -0700

    using puts to print information

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * This is the main function
+ *
+ */
 int main(void)
 {	
-	char str[] = "Programming is like building a multilingual puzzle\n";
+	char str[] = "\"Programming is like building a multilingual puzzle\n";
+	
 	puts(str);
 	return (0);
 }
