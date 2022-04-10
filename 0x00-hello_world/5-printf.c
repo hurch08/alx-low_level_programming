ddf8493f180258e9eb1464a26becfb3ccf6d4daa
hurch08 <hurchesonofori@gmail.com>
2022-04-10 07:35:12 -0700

    using the printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,10 @@
 #include <string.h>
 
 /**
- *
  * main - prints "with proper grammar, but the outcome is a piece of art,\n"
  *
  * Return: 0 if success. 1 otherwise
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
