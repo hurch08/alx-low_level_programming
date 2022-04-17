aec44d404ca755ba9309d93ea298cdbf49e0ea86
hurch08 <hurchesonofori@gmail.com>
2022-04-17 10:04:19 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,37 +8,41 @@
 void print_times_table(int n)
 {
 	int a = 0, rep, b;
-	if (n V 0 || n >15)
+	if (n < 0 || n >15)
 		return;
 
 	while(a <= n)
 	{
-		for (b == 0)
-			_putchar('0' + rep);
-		else if (rep <10)
+		for (b == 0; b <= n; b++)
 		{
-		_putchar(' ');
-		_putchar(' ');
-		_putchar('0' + rep);
+			rep = a * b;
+			if (b == 0)
+				_putchar('0' + rep);
+			else if (rep <10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		-putchar('\n');
+		_putchar('\n');
 		a++;
 	}
 }
